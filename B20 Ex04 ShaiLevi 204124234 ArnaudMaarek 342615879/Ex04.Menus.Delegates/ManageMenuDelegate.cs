261af928aefb8ae1be244818e526a57bf7adbdc3
Shai Levi <58184723+shailevi23@users.noreply.github.com>
2021-11-08 11:54:57 +0200

    Add files via upload

---
+++ B20 Ex04 ShaiLevi 204124234 ArnaudMaarek 342615879/Ex04.Menus.Delegates/ManageMenuDelegate.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ex04.Menus.Delegates
+{
+    public class ManageMenuDelegate
+    {
+        public void RunMenu(MenuItem i_MainMenu)
+        {
+            MenuItem currentMenuItem, previousMenuItem;
+            int userChoice;
+            bool isMenu = true;
+            currentMenuItem = i_MainMenu;
+            previousMenuItem = currentMenuItem;
+
+            while (isMenu)
+            {
+                try
+                {
+                    currentMenuItem.Show(currentMenuItem.SubMenu, currentMenuItem.Level, currentMenuItem.Title);
+                    userChoice = currentMenuItem.ReadInput(currentMenuItem.SubMenu.Count);
+                    if (!CheckCurrentMenuItemAction(userChoice, ref currentMenuItem, ref previousMenuItem))
+                    {
+                        isMenu = false;
+                    }
+                    else
+                    {
+                        Console.Clear();
+                    }
+                }
+                catch (FormatException fe)
+                {
+                    Console.WriteLine("The input: " + fe.Message + " is not a number" + Environment.NewLine);
+                    Thread.Sleep(2000);
+                    Console.Clear();
+                }
+                catch (ArgumentOutOfRangeException aoore)
+                {
+                    Console.WriteLine("The input: " + aoore.ParamName + " is out of range" + Environment.NewLine);
+                    Thread.Sleep(2000);
+                    Console.Clear();
+                }
+            }
+        }
+
+        public bool CheckCurrentMenuItemAction(int i_UserChoice, ref MenuItem io_CurrentMenu, ref MenuItem io_PreviusMenu)
+            {
+                bool checkIsMenu = true;
+
+                if (i_UserChoice == 0)
+                {
+                    if (io_CurrentMenu.Level != 1)
+                    {
+                        io_CurrentMenu = io_PreviusMenu;
+                    }
+                    else
+                    {
+                        io_CurrentMenu = io_CurrentMenu.SubMenu[0];
+                        io_CurrentMenu.ItemFunctionWasClicked();
+                        checkIsMenu = false;
+                    }
+                }
+                else
+                {
+                    io_PreviusMenu = io_CurrentMenu;
+                    io_CurrentMenu = io_CurrentMenu.SubMenu[i_UserChoice];
+                    if (io_CurrentMenu.SubMenu.Count == 0)
+                    {
+                        io_CurrentMenu.ItemFunctionWasClicked();
+                        checkIsMenu = false;
+                    }
+                }
+
+                return checkIsMenu;
+            }
+    }
+}
---
+++ B20 Ex04 ShaiLevi 204124234 ArnaudMaarek 342615879/Ex04.Menus.Delegates/MenuItem.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex04.Menus.Delegates
+{
+     public delegate void ItemWasClicked();
+
+     public class MenuItem
+     {
+          protected List<MenuItem> m_Menus = new List<MenuItem>();
+          protected string m_Title;
+          protected int m_Level;
+          private readonly string m_Enter = Environment.NewLine;
+
+          public event ItemWasClicked MenuItemClickInvoker;
+
+          public int Level
+          {
+               get { return m_Level; }
+               set { m_Level = value; }
+          }
+
+          public string Title
+          {
+               get { return m_Title; }
+               set { m_Title = value; }
+          }
+
+          public List<MenuItem> SubMenu
+          {
+               get { return m_Menus; }
+               set { m_Menus = value; }
+          }
+
+          public void Show(List<MenuItem> i_MenuItems, int i_Level, string i_Title)
+          {
+               int counterItems = 0;
+               Console.WriteLine(m_Enter + "Menu Level: " + i_Level + m_Enter + i_Title + m_Enter);
+               Console.WriteLine("Please choose an option: ");
+
+               foreach (MenuItem menuItem in i_MenuItems)
+               {
+                    Console.WriteLine(counterItems + ". " + menuItem.Title);
+                    counterItems++;
+               }
+          }
+
+          public int ReadInput(int i_Size)
+          {
+               int choiceInput;
+               string input = Console.ReadLine();
+               bool checkInput = int.TryParse(input, out choiceInput);
+               if (!checkInput)
+               {
+                    throw new FormatException(input);
+               }
+               else
+               {
+                    if (choiceInput < 0 || choiceInput >= i_Size)
+                    {
+                        throw new ArgumentOutOfRangeException(choiceInput.ToString());
+                    }
+               }
+
+               return choiceInput;
+          }
+
+          public void ItemFunctionWasClicked()
+          {
+               OnItemWasClicked();
+          }
+
+          protected virtual void OnItemWasClicked()
+          {
+               if (MenuItemClickInvoker != null)
+               {
+                    MenuItemClickInvoker.Invoke();
+               }
+          }
+     }
+}
